24fb5b6008c1890ee0ae16f8a0af4670a7170e44
apizzimenti <apizzime@gmu.edu>
2023-06-28 12:57:11 -0400

    new default experiment file

--- experiments/.experiment.py
+++ experiments/.experiment.py
@@ -1,20 +1,30 @@
 
+import pandas as pd
+import numpy as np
+import json
 from potts import Lattice, SwendsonWang, Chain
-from potts.stats import critical
+from potts.stats import randomizedToConstant
 from potts.utils import Metadata
 
 # Set the order of our coefficient field and the size of the lattice.
-q = 2
-corners = [100,100]
+q = 7
+corners = [20, 20]
+steps = 1000
 
-# Create a 100x100 integer lattice to experiment on.
+# Create an integer lattice to experiment on.
 lattice = Lattice(corners=corners, field=q)
-model = SwendsonWang(temperature=critical(q))
+schedule = randomizedToConstant(steps=steps, field=q, distribution=np.random.normal)
+model = SwendsonWang(temperature=schedule)
 initial = model.initial(lattice)
 
 # Create a chain and iterate, stashing information as we go.
-chain = Chain(lattice, model, initial=initial)
+chain = Chain(lattice, model, initial=initial, sampleInterval=50, steps=steps)
 
 # Step through the chain.
 with Metadata(chain) as metadata:
     for step in chain.progress(): pass
+
+# Write statistics to file.
+# Write statistics to file.
+pd.DataFrame.from_dict(chain.statistics).to_csv("./output/statistics/energy.csv")
+with open("./output/statistics/assignments.json", "w") as w: json.dump(chain.assignments, w)
